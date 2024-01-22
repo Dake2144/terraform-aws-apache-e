3b39c75afdd561d204eef6f364667a93e298ae6e
Daulet Sametbay <sametbay.i5@gmail.com>
2024-01-22 22:38:55 +0600

    should use subnet specified from the vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-
 data "aws_vpc" "main" {
   id = var.vpc_id
+}
+
+data "aws_subnets" "subnet_ids" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+
 }
 
 resource "aws_key_pair" "deployer" {
@@ -60,10 +67,13 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
+
+
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.amazon-linux-2.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.deployer.key_name
+  subnet_id              = tolist(data.aws_subnets.subnet_ids.ids)[0]
   vpc_security_group_ids = [aws_security_group.sg_my_server.id]
   user_data              = data.template_file.user_data.rendered
 
